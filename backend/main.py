2dd582740d7aec2490905613f09262064a9169cb
YJ <yjia0405@gmail.com>
2025-12-21 17:01:21 -0500

    fix

--- backend/main.py
+++ backend/main.py
@@ -167,6 +167,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex="https://.*\.railway\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
