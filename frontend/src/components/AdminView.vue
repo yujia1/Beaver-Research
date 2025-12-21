a19c56706bc389e2ee66c8c5c3b7c4b7fd1e4463
YJ <yjia0405@gmail.com>
2025-12-21 15:10:30 -0500

    Add /academy to access management resources

--- frontend/src/components/AdminView.vue
+++ frontend/src/components/AdminView.vue
@@ -638,7 +638,7 @@ const permissions = ref([])
 const loadingPermissions = ref(false)
 const permissionsError = ref('')
 const roles = ['admin', 'creator', 'contributor', 'user']
-const resourceTypes = ['/research', '/alphatrade', '/report', '/investment', '/short-interest', '/whale-watching', '/agent']
+const resourceTypes = ['/research', '/alphatrade', '/report', '/investment', '/short-interest', '/whale-watching', '/agent', '/academy']
 
 // Message State
 const message = ref('')
